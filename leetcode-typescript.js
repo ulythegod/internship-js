2aa785685e5f944b8026d06f5fe13783314dc77a
Yulia Skorobogatova <sjs@smart.inreco.ru>
2022-02-08 17:45:26 +0300

    typescreept learning and leetcode

--- leetcode-typescript.js
+++ leetcode-typescript.js
@@ -13,5 +13,53 @@ function checkIfExist(arr) {
     return result;
 }
 ;
-var arr = [3, 1, 7, 11];
-console.log(checkIfExist(arr));
+// let arr = [3, 1, 7, 11];
+// checkIfExist(arr);
+//Valid Mountain Array
+function validMountainArray(arr) {
+    var result = false;
+    if (arr.length >= 3) {
+        var n = arr.length;
+        var i = 0;
+        while (i + 1 < n && arr[i] < arr[i + 1]) {
+            i++;
+        }
+        if (i == 0 || i == n - 1) {
+            return result;
+        }
+        while (i + 1 < n && arr[i] > arr[i + 1]) {
+            i++;
+        }
+        result = (i == (n - 1));
+    }
+    return result;
+}
+;
+// let arr = [0,3,2,1];
+// console.log(validMountainArray(arr));
+//Replace Elements with Greatest Element on Right Side
+function replaceElements(arr) {
+    arr.forEach(function (value, index) {
+        console.log("index", index);
+        console.log("value", value);
+        if (index == arr.length - 1) {
+            arr[index] = -1;
+        }
+        else {
+            var maxRight = arr[index + 1];
+            for (var i = index + 1; i < arr.length; i++) {
+                if (arr[i] > maxRight) {
+                    maxRight = arr[i];
+                }
+            }
+            console.log("maxRight", maxRight);
+            arr[index] = maxRight;
+        }
+        console.log("new", arr[index]);
+        console.log("");
+    });
+    return arr;
+}
+;
+var arr = [57010, 40840, 69871, 14425, 70605];
+console.log(replaceElements(arr));
--- leetcode-typescript.ts
+++ leetcode-typescript.ts
@@ -16,5 +16,59 @@ function checkIfExist(arr: number[]): boolean {
     return result;
 };
 
-let arr = [3, 1, 7, 11];
-checkIfExist(arr);
+// let arr = [3, 1, 7, 11];
+// checkIfExist(arr);
+
+//Valid Mountain Array
+function validMountainArray(arr: number[]): boolean {
+    let result = false;
+
+    if (arr.length >= 3) {
+        let n = arr.length;
+        let i = 0;
+        
+
+        while (i + 1 < n && arr[i] < arr[i+1]){
+            i++;
+        }
+
+        if (i == 0 || i == n - 1) {
+            return result;
+        }
+
+        while (i+1 < n && arr[i] > arr[i+1]) {
+            i++;
+        }
+
+        result = (i == (n - 1));
+    }
+
+    return result;
+};
+
+// let arr = [0,3,2,1];
+// console.log(validMountainArray(arr));
+
+//Replace Elements with Greatest Element on Right Side
+function replaceElements(arr: number[]): number[] {
+    arr.forEach(function (value, index) {        
+        if (index == arr.length - 1) {
+            arr[index] = -1;
+        } else {
+            let maxRight = arr[index + 1];
+            
+            for (let i = index + 1; i < arr.length; i++) {
+                if (arr[i] > maxRight) {
+                    maxRight = arr[i];
+                }
+            }
+
+            arr[index] = maxRight;
+        }
+    });
+
+    return arr;
+};
+
+let arr = [57010, 40840, 69871, 14425, 70605];
+console.log(replaceElements(arr));
