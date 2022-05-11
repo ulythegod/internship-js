266f3d3a2215e9433dbced13837052fec28b4679
Yulia Skorobogatova <sjs@smart.inreco.ru>
2022-05-11 13:07:48 +0300

    leetcode tasks

---
+++ leetcodecommon.js
@@ -0,0 +1,58 @@
+//Two Sum
+// function twoSum(nums: number[], target: number): number[] {
+//     let result: number[] = [];
+//     let firstId: number = 0;
+//     let secondId: number = 0;
+//     for (let i = 0; i < nums.length; i++) {
+//         firstId = i;
+//         for(let j = nums.length - 1; j >= 0; j--) {            
+//             secondId = j;
+//             if (nums[firstId] + nums[secondId] === target && i !== j) {
+//                 result = [firstId, secondId];
+//                 break;
+//             }
+//         }
+//         if (result.length === 2) {
+//             break;
+//         }
+//     }
+//     return result;
+// };
+// console.log(twoSum([2, 7, 11, 15], 9));
+// console.log(twoSum([3, 2, 4], 6));
+// console.log(twoSum([3, 3], 6));
+//Median of Two Sorted Arrays
+// function findMedianSortedArrays(nums1: number[], nums2: number[]): number {
+//     let res: number = 0;
+//     let mergedArr: number[] = nums1.concat(nums2);
+//     mergedArr.sort((a: number, b: number) => {
+//         if (a < b) {
+//             return -1;
+//         }
+//     });  
+//     if (mergedArr.length % 2 !== 0) {
+//         let middleIndex = Math.floor(mergedArr.length/2);
+//         res = mergedArr[middleIndex];       
+//     } else {
+//         let firstMiddleIndex = mergedArr.length/2;
+//         res = (mergedArr[firstMiddleIndex] + mergedArr[firstMiddleIndex - 1])/2;    
+//     }
+//     return res;
+// };
+// console.log(findMedianSortedArrays([1,3], [2]));
+// console.log(findMedianSortedArrays([1,3], [2,7]));
+// console.log(findMedianSortedArrays([3], [-2, -1]));
+//Palindrome Number
+function isPalindrome(x) {
+    var res = false;
+    console.log(x.toString());
+    console.log(x.toString().split("").reverse().join());
+    if (x.toString() === x.toString().split("").reverse().join("")) {
+        res = true;
+    }
+    return res;
+}
+;
+console.log(isPalindrome(121));
+//console.log(isPalindrome(-121));
+//console.log(isPalindrome(10));
